da70c60d582c316bc8762d9b9cdd4da642f96568
NICOLAS <72447720+MRCnicolas@users.noreply.github.com>
2021-03-10 18:29:51 +0100

    Depot10mars

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -6,17 +6,19 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('name')
             ->add('email')
             //->add('roles')
-            ->add('password')
-            ->add('confirm_password')
-            ->add('name')
+            ->add('password', PasswordType::class)
+            ->add('confirm_password', PasswordType::class)
+            
         ;
     }
 
