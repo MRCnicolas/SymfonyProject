a5f96e8ecb992db5731a084076d83d5f8d5a12c0
NICOLAS <72447720+MRCnicolas@users.noreply.github.com>
2021-04-11 09:55:50 +0200

    Depot11avril

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -39,7 +39,7 @@ class ContactController extends AbstractController
 
         return $this->render('contact/index.html.twig', [
             'contact_form' => $form->createView(),
-            'title' => 'Formulaire de contact',
+            'title' => 'Contactez-nous',
         ]);
     }
 }
